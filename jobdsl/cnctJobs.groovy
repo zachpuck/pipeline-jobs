9386701c4b46af6c7d8a2a7526d22d4d296116f4
Marat G <maratoid@gmail.com>
2018-03-12 14:52:36 -0700

    use configure block for github traits

--- jobdsl/cnctJobs.groovy
+++ jobdsl/cnctJobs.groovy
@@ -81,16 +81,14 @@ void createJobs() {
       }
 
       configure { project ->
-        project / 'sources' / 'data' / 'jenkins.branch.BranchSource'/ source(class: 'org.jenkinsci.plugins.github_branch_source.GitHubSCMSource') {
-          traits() {
-            'org.jenkinsci.plugins.github__branch__source.BranchDiscoveryTrait'() {
-              strategyId(3)
-            }
-            
-            'org.jenkinsci.plugins.github__branch__source.ForkPullRequestDiscoveryTrait'() {
-              strategyId(1)
-              trust(class: 'org.jenkinsci.plugins.github_branch_source.ForkPullRequestDiscoveryTrait$TrustPermission')
-            }
+        project / 'sources' / 'data' / 'jenkins.branch.BranchSource'/ source(class: 'org.jenkinsci.plugins.github_branch_source.GitHubSCMSource') / traits {
+          'org.jenkinsci.plugins.github__branch__source.BranchDiscoveryTrait'() {
+            strategyId(3)
+          }
+          
+          'org.jenkinsci.plugins.github__branch__source.ForkPullRequestDiscoveryTrait'() {
+            strategyId(1)
+            trust(class: 'org.jenkinsci.plugins.github_branch_source.ForkPullRequestDiscoveryTrait$TrustPermission')
           }
         }
       }
